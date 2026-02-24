a45a7a65e97d84629b1cd924e9a39527da0bab3e
naumanrazzaq-dot <maliknomigep@gmail.com>
2026-02-24 20:11:46 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter a number:" ;
+    cin>> a ;
+    int i=1;
+    while( i<=n )
+        cout<<"*"<<endl:
+            i=i+1;
+    int j=1;
+    while(j<=n)
+        cout<<"*"<<endl;
+    j=j+1;
+    return 0;
+}
